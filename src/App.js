76bbfb64828a747cc57e499fb4715bfbd27c7f6d
jaime <jaimepinkrah@gmail.com>
2022-03-24 13:47:55 -0700

    React Component 4

--- src/App.js
+++ src/App.js
@@ -1,46 +1,66 @@
 import logo from "./logo.svg";
 import "./App.css";
 //import writers from "./writers";
-import { ProfileCard } from "./ProfileCard";
+import ProfileCard from "./ProfileCard";
 import { useEffect, useState } from "react";
 
 function App() {
-  const [writers, setWriters] = useState([]);
- 
-  useEffect (() => {
-    const getWriters = async () => {
-      const response = await fetch("./writers.json")
-      const data = await response.json()
-      setWriters(data)
-    }
-    getWriters()
-  }, [])
+  const [data, setData] = useState({
+    writers: [],
+    loading: false,
+  });
+
+  const handleClick = () => {
+    setData(prevData => ({
+      ...prevData,
+      loading: true
+    }))
+
+    setTimeout(() => {
+      const getWriters = async () => {
+        const response = await fetch("./writers.json");
+        const data = await response.json();
+        setData({
+          writers: data,
+          loading: false
+        });
+      };
+      getWriters();
+    }, 2000);
+  };
+
+  if(data.loading){
+    return(
+      <div>
+        <h1>Writer Profiles</h1>
+        <div className="container">
+          <div className="card ation">
+            <p className="infoText">Loading...</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
   return (
     <div>
       <h1>Writer Profiles</h1>
 
       <div className="container">
-        {writers.map((writer) => (
-          <ProfileCard writer={writer}/>
-
-          // <div className="card">
-          //   <img
-          //     src={`/images/${writer.avatar}.png`}
-          //     height="300px"
-          //     width="300px"
-          //     alt=""
-          //   />
-
-          //   <div className="textGroup">
-          //     <h3>{writer.name}</h3>
-          //     <p>{writer.email}</p>
-          //     <p>{writer.phone}</p>
-          //   </div>
-          // </div>
-        ))}
+        {data.writers.length=== 0 ? (
+          <div className="card action"> 
+          <p className="infoText">Oops... no writer profile found</p>
+          <button className="actionBtn" onClick={handleClick}>Get Writers</button>
+          </div>
+        ) : (
+          data.writers.map((writer) => (
+            <ProfileCard key={writer.id} writer={writer}/>
+          ))
+        )}
       </div>
-    </div>
-  );
-}
+      </div>
+    )
+  }
+
 
 export default App;
